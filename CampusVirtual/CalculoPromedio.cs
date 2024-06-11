28ca5bc6473009a72195d0b9ff2e057b4562ec65
Pedro <ingenierocastroucc@gmail.com>
2024-06-11 14:14:57 -0500

    Push inicial del proyecto CampusVirtual C

---
+++ CampusVirtual/CalculoPromedio.cs
@@ -0,0 +1,51 @@
+﻿#region Documentación
+/****************************************************************************************************
+ * Propiedad intelectual Pedro castro                                                           
+ ****************************************************************************************************
+ * Unidad        : <CalculoPromedioController>                                                                      
+ * DescripciÓn   : <Controlador para registrar y mostrar el promedio de calificaciones>                                                      
+ * Autor         : <Pedro Castro>
+ * Fecha         : <17-08-2017>                                                                             
+ *                                                                                                           
+ * Fecha         Autores         	Modificación                                                                 
+ * ===========   ============   	=====================
+ * 11-06-2024   <Pedro Castro>        1 - Version Inicial  
+ ***************************************************************************************************/
+#endregion Documentación
+
+double PrimerCorte;
+double SegundoCorte;
+double TercerCorte;
+double CuartoCorte;
+string Promedioanual;
+string PromedioSemestral;
+
+/// <summary>
+/// Capturar y ver detalle del promedio semestral
+/// </summary>
+/// <returns>listas del detalle de la solicitud de servicios</returns>
+Console.WriteLine("Tu promedio actual semestral:");
+Console.WriteLine("Ingresa el valor de tu primer corte:");
+PrimerCorte = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa el valor de tu segundo corte:");
+SegundoCorte = Convert.ToDouble(Console.ReadLine());
+PromedioSemestral = ("Tu promedio del semestre es:" + PrimerCorte + SegundoCorte / 2);
+
+Console.WriteLine(PromedioSemestral);
+
+/// <summary>
+/// Capturar y ver detalle del promedio anual
+/// </summary>
+/// <returns>listas del detalle de la solicitud de servicios</returns>
+Console.WriteLine("Tu promedio actual");
+Console.WriteLine("Ingresa el valor de tu primer corte:");
+PrimerCorte = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa el valor de tu segundo corte:");
+SegundoCorte = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa el valor de tu tercer corte:");
+TercerCorte = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa el valor de tu cuarto corte:");
+CuartoCorte = Convert.ToDouble(Console.ReadLine());
+Promedioanual = ("Tu promedio anual es:" + PrimerCorte + SegundoCorte + TercerCorte + CuartoCorte / 4);
+
+Console.WriteLine(Promedioanual);
---
+++ README.md
@@ -0,0 +1,17 @@
+# CampusVirtual
+Contiene el codigo utilizado para el proyecto de campus virtual
+> Esta version inicial de la aplicacion web del CampusVirtual sera la base para todo el desarrollo completo del proyecto.
+> - developer
+
+## En este repo podras encontrar contenido C#,Html,CSS,Git y .Net
+* Todos los comandos de Git
+* El flujo de trabajo en Github
+* Buenas prácticas
+* Fundamentos y conocimientos especializados en .Net
+* Fundamentos y conocimientos especializados en HTML
+* Fundamentos y conocimientos especializados en CSS
+* Fundamentos y conocimientos especializados en  C#
+
+
+Resibira actualizaciones semanales o diarias: **Este readme.md se realiza con la finalidad de informar**.
+
