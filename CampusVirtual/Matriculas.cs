41ae3a5bea058495bc4187ca7e74784406356b2d
Pedro <ingenierocastroucc@gmail.com>
2024-06-15 19:09:22 -0500

    Se crean clases y objetos, para las matriculas, asignaturas y profesores

--- CampusVirtual/Matriculas.cs
+++ CampusVirtual/Matriculas.cs
@@ -2,20 +2,47 @@
 /****************************************************************************************************
  * Propiedad intelectual Pedro castro                                                           
  ****************************************************************************************************
- * Unidad        : <MatriculasController>                                                                      
- * DescripciÓn   : <Controlador para establecer las clases de las matriculas>                                                      
+ * Unidad        : <Matriculas>                                                                      
+ * DescripciÓn   : <Clase para establecer las clases de las matriculas>                                                      
  * Autor         : <Pedro Castro>
  * Fecha         : <14-06-2024>                                                                             
  *                                                                                                           
  * Fecha         Autores         	Modificación                                                                 
  * ===========   ============   	=====================
+ * 15-06-2024   <Pedro Castro>        1- Se crean las clases Matriculas, Profesor y Asiganturas.
  ***************************************************************************************************/
 #endregion Documentación
 
 class Matriculas
 {
+    public int IdAsignaturamatriculada;
+    public string NombreAsignatura;
+    public bool Nivelacion;
+    public List<Asignaturas> IntencidadHoraria;
+    public string Profesor;
+}
+
+class Profesor
+{
+    public int IdProfesor;
     public string Nombre;
     public bool Nivelacion;
     public int IntencidadHoraria;
-    public string Profesor;
+    public string Asignatura;
+}
+
+class Asignaturas
+{
+    public int IdAsignatura;
+    public string Nombre;
+    public bool Nivelacion;
+    public IntencidadHoraria Horario;
+    public string Asignatura;
+}
+
+enum IntencidadHoraria
+{ 
+    Cuatro,
+    Dos,
+    Tres
 }
